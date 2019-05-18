bc44ea851245f6d4609c685735a6a96715d7c1d5
Oleg Marshalenko <oleg@firejack.net>
2019-05-18 23:43:36 +0300

    Added BootLoad tool

---
+++ utils/flashme.bat
@@ -0,0 +1,3 @@
+tools\esptool.exe --chip esp32 --port %1 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_freq 80m --flash_mode dio --flash_size 4MB 0x1000 firmware\bootloader.bin 0x8000 firmware\partitions.bin 0xe000 firmware\boot_app0.bin 0x10000 firmware\firmware.bin
+putty.exe -serial %1 -sercfg 115200 8,n,1,N
+
