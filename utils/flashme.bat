7638cc96266a2a068a88f245a215e4c41afc1f29
Oleg Marshalenko <oleg.marshalenko@gmail.com>
2020-04-19 18:23:05 +0300

    added old and new versions of software

--- utils/flashme.bat
+++ utils/flashme.bat
@@ -1,3 +1,1 @@
-tools\esptool.exe --chip esp32 --port %1 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_freq 80m --flash_mode dio --flash_size 4MB 0x1000 firmware\bootloader.bin 0x8000 firmware\partitions.bin 0xe000 firmware\boot_app0.bin 0x10000 firmware\firmware.bin 0x291000 firmware\spiffs.bin
-toots\putty.exe -serial %1 -sercfg 115200 8,n,1,N
-
+tools\esptool.exe --chip esp32 --port %1 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_freq 80m --flash_mode dio --flash_size 4MB 0x0000 firmware\bootloader.bin 0x10000 firmware\firmware.bin 0x290000 firmware\spiffs.bin
--- utils/flashme_old.sh
+++ utils/flashme_old.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 tools/esptool.py --chip esp32 --port $1 --baud 921600 erase_flash
 tools/esptool.py --chip esp32 --port $1 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_freq 80m --flash_mode dio --flash_size 4MB \
-0x0000 firmware/bootloader.bin \
-0x10000 firmware/firmware.bin \
-0x290000 firmware/spiffs.bin
+0x1000 firmware_old/bootloader.bin \
+0x8000  firmware_old/partitions.bin \
+0xe000  firmware_old/boot_app0.bin \
+0x10000  firmware_old/firmware.bin
+0x291000 firmware/spiffs.bin
